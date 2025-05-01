b39b2e49a5cf2718e274720e57f5fa689574f231
lukin921 <lukin921@student.liu.se>
2025-05-01 19:21:55 +0200

    +1

---
+++ 10970-big-chocolate/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(false);
+
+    int M, N;
+    while (cin >> M >> N) {
+        cout << M * N - 1 << '\n';
+    }
+
+    return 0;
+}
